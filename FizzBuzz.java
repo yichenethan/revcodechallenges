b94aef0864e7ccb1f82e64e30323e981c7f0e81c
ethan <yichen12@uw.edu>
2022-08-19 10:08:13 -0700

    fizzbuzz

---
+++ FizzBuzz.java
@@ -0,0 +1,26 @@
+public class FizzBuzz {
+  public static void main(String[] args) {
+    String result = fizzBuzz(100);
+    System.out.println(result);
+  }
+  public static String fizzBuzz(int nums) {
+    StringBuilder sb = new StringBuilder();
+    for(int i = 1; i <= nums; i++) {
+      if(i % 3 == 0) {
+        sb.append("Fizz");
+      }
+      if(i % 5 == 0) {
+        sb.append("Buzz");
+      }
+      if(i % 5 != 0 && i % 3 != 0) {
+        sb.append(i);
+      }
+      //if(i != nums) {
+        sb.append(", ");
+      //}
+    }
+    sb.delete(sb.length() - 2, sb.length());
+    return sb.toString();
+  }
+}
+
--- PigLatin.java
+++ PigLatin.java
@@ -2,6 +2,7 @@ public class PigLatin {
   public static void main(String[] args) {
     System.out.println(pLatin("Revature"));
     System.out.println(pLatinBuild("Makima"));
+    System.out.println(encode("I want to be Makimas dog."));
   }
 
   //Strings are not mutable - this is making new objects!
@@ -19,5 +20,28 @@ public class PigLatin {
     sb.append(word.charAt(0));
     sb.append("ay");
     return sb.toString();
+  }
+
+  public static String encode(String sentence) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(sentence);
+    char punctuation = sb.charAt(sb.length() - 1);
+
+
+    sb.deleteCharAt(sb.length() - 1 );
+
+    if(sb.toString().contains("\\p{Punct}")) {
+      return("Invalid stuff");
+    }
+    String[] words = sb.toString().split(" ");
+    StringBuilder reBuild = new StringBuilder();
+    for (String word: words) {
+      reBuild.append(pLatinBuild(word));
+      reBuild.append(" ");
+    }
+    reBuild.deleteCharAt(reBuild.length() - 1);
+    reBuild.append(punctuation);
+
+    return reBuild.toString();
   }
 }
